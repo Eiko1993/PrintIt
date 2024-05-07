78ced4886b6e4e00fe6e25381b8e30ee2675c8b6
Vincent Merah <vincent.merah@gmail.com>
2024-05-07 15:56:22 +0200

    fleche

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,82 @@
-let arrow_left = document.getElementById("#arrowleft");
-let arrow_right = document.getElementById("#arrowright");
+const arrowleft = document.querySelector(".arrow_left");
+const arrowright = document.querySelector(".arrow_right");
+let banner = document.querySelector(".banner_img");
+let tagline = document.querySelector(".tagline");
+
+const dots = document.querySelector(".dots");
 
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		image:"slide1.jpg",
+		tagLine:"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		image:"slide2.jpg",
+		tagLine:"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		image:"slide3.jpg",
+		tagLine:"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		image:"slide4.png",
+		tagLine:"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
 
-function move_left() {
+arrowleft.onclick = move_left();
+arrowright.onclick = move_right();
+
+
+
+function move_left(i) {
+	console.log("gauche");
+	arrowleft.addEventListener("click", move_left);
+	/**slides[i=i-1];
+	if (slides[i=0]){
+		banner.src = "./assets/images/slideshow/slide1.jpg"
+		tagline.p = "Impressions tous formats <span>en boutique et en ligne</span>"
+		}
+	if (slides[i=1]){
+		banner.src = "./assets/images/slideshow/slide2.jpg"
+		tagline.p = "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		}
+	if (slides[i=2]){
+		banner.src = "./assets/images/slideshow/slide3.jpg"
+		tagline.p = "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		}
+	if (slides[i=3]){
+		banner.src = "./assets/images/slideshow/slide4.jpg"
+		tagline.p = "Autocollants <span>avec découpe laser sur mesure</span>"
+		}
+	}*/
+
+
 
 }
 
-function move_right() {
-
+function move_right(i) {
+console.log("droite");
+arrowright.addEventListener("click", move_right);
+	/**slides[i=i+1];
+	if (slides[i=0]){
+		banner.src = "./assets/images/slideshow/slide1.jpg"
+		tagline.p = "Impressions tous formats <span>en boutique et en ligne</span>"
+		}
+	if (slides[i=1]){
+		banner.src = "./assets/images/slideshow/slide2.jpg"
+		tagline.p = "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		}
+	if (slides[i=2]){
+		banner.src = "./assets/images/slideshow/slide3.jpg"
+		tagline.p = "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		}
+	if (slides[i=3]){
+		banner.src = "./assets/images/slideshow/slide4.jpg"
+		tagline.p = "Autocollants <span>avec découpe laser sur mesure</span>"
+		}
+	}*/
 }
 
-arrow_left.onclick = move_left();
 
-arrow_right.onclick = move_right();
